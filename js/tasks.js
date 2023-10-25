a86d208165a13d377214e7bc20e5af90127a9f44
tobiastermer <tobiastermer@gmx.de>
2023-10-25 23:14:40 +0200

    js for tasks first commit

--- js/tasks.js
+++ js/tasks.js
@@ -1,3 +1,5 @@
+let tasks = [];
+
 function showAddTaskOverlay() {
     document.getElementById("addTaskOverlay").style.display = "flex";
     document.getElementById("addTask-close-button").style.display = "flex";
@@ -6,4 +8,12 @@ function showAddTaskOverlay() {
 function closeAddTaskOverlay() {
     document.getElementById("addTaskOverlay").style.display = "none";
     document.getElementById("addTask-close-button").style.display = "none";
+}
+function addNewTask() {
+
+    let title = document.getElementById('addTaskTitle').value
+    let description = document.getElementById('addTaskDescription').value
+    // assigned to noch ergänzen
+    let dueDate = document.getElementById('addTaskDueDate').value
+    
 }
