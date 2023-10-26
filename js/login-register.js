3c8056d5bdb50d2a1aea3e854debc8c7854652ad
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2023-10-26 19:20:36 +0200

    added some additions to login/register js

--- js/login-register.js
+++ js/login-register.js
@@ -1,21 +1,23 @@
-let users = [ //Testwise array of users
-    {'email': 'pierce@test.de', 'password': 'test123'}
-];
+let users = [];
 
 //Register user
-function addUser() {
-    console.log('TEST')
-    let name = document.getElementById('name');
-    let email = document.getElementById('email');
-    let password = document.getElementById('password');
-    users.push({'email': email.value, 'password': password.value, 'name': name.value});
-    // Weiterleitung zu Login Seite + Nachricht anzeigen: "Erfolgreiche Registration"
-    window.location.href = 'login.html?msg=Du hast dich erfolgreich registriert';
+
+async function register() {
+    registerBtn.disabled = true;
+    users.push({
+        email: email.value,
+        password: password.value,
+    });
+    await setItem('users', JSON.stringify(users));
+    resetForm();
+     // Weiterleitung zu Login Seite + Nachricht anzeigen: "Erfolgreiche Registration"
+    //  window.location.href = 'login.html?msg=Du hast dich erfolgreich registriert';
 }
+
+
 
 //Login user 
 function login() {
-    debugger;
     let email = document.getElementById('email');
     let password = document.getElementById('password');
     let user = users.find( u => u.email == email.value && u.password == password.value);
@@ -36,17 +38,6 @@ async function loadUsers(){
     } catch(e){
         console.error('Loading error:', e);
     }
-}
-
-
-async function register() {
-    registerBtn.disabled = true;
-    users.push({
-        email: email.value,
-        password: password.value,
-    });
-    await setItem('users', JSON.stringify(users));
-    resetForm();
 }
 
 function resetForm() {
