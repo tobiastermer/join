1de076b626648402c7b37aa00092e1001cc65540
tobiastermer <tobiastermer@gmx.de>
2023-10-26 06:36:59 +0200

    js for choosing prio in add task finished

--- js/tasks.js
+++ js/tasks.js
@@ -15,11 +15,21 @@ function addNewTask() {
     let description = document.getElementById('addTaskDescription').value
     // assigned to noch ergänzen
     let dueDate = document.getElementById('addTaskDueDate').value
-    
+
 }
 
 function setPrio(prio) {
+    // List of possible priorities
+    const prios = ['high', 'med', 'low'];
 
-    document.getElementById(`btn-prio-${prio}`).style.backgroundColor = ''
+    // Remove 'active' class from all buttons
+    prios.forEach(p => {
+        document.getElementById(`img-prio-${p}`).classList.remove(`prio-${p}-active`);
+        document.getElementById(`btn-prio-${p}`).classList.remove(`bg-prio-${p}-active`);
+    });
 
+    // Add 'active' class to the selected button
+    document.getElementById(`img-prio-${prio}`).classList.add(`prio-${prio}-active`);
+    document.getElementById(`btn-prio-${prio}`).classList.add(`bg-prio-${prio}-active`);
 }
+
