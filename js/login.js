1332d9c9ff062836e10f5c6f0882afd224581fff
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2023-10-27 21:11:03 +0200

    final changes to register form

---
+++ js/login.js
@@ -0,0 +1,24 @@
+//Login user 
+function login() {
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let user = users.find(u => u.email == email.value && u.password == password.value);
+    console.log(user);
+    if (user) {
+        console.log('User gefunden')
+    }
+}
+
+//Load user
+async function init() {
+    loadUsers();
+}
+
+async function loadUsers() {
+    try {
+        users = JSON.parse(await getItem('users'));
+    } catch (e) {
+        console.error('Loading error:', e);
+    }
+}
+
--- js/register.js
+++ js/register.js
@@ -16,32 +16,7 @@ async function register() {
     //  window.location.href = 'login.html?msg=Du hast dich erfolgreich registriert';
 }
 
-
-
-//Login user 
-function login() {
-    let email = document.getElementById('email');
-    let password = document.getElementById('password');
-    let user = users.find(u => u.email == email.value && u.password == password.value);
-    console.log(user);
-    if (user) {
-        console.log('User gefunden')
-    }
-}
-
-//Load user
-async function init() {
-    loadUsers();
-}
-
-async function loadUsers() {
-    try {
-        users = JSON.parse(await getItem('users'));
-    } catch (e) {
-        console.error('Loading error:', e);
-    }
-}
-
+// Reset form after submit
 function resetForm() {
     userName.value = '';
     email.value = '';
@@ -49,7 +24,6 @@ function resetForm() {
     PWconfirm.value = '';
     registerBtn.disabled = false;
 }
-
 
 // Visual Functions
 function togglePasswordEye() {
