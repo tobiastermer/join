81ca28b47eefef7bee82bb447ebe9425663b4f08
tobiastermer <tobiastermer@gmx.de>
2023-10-30 20:09:56 +0100

    update

--- js/board.js
+++ js/board.js
@@ -7,11 +7,11 @@
 // ****************
 
 // Global variables
-
+let board;
 
 
 // Array for different progress levels
-let progressbar = [
+let progresses = [
     "To Do",
     "In Progress",
     "Await Feedback",
@@ -25,16 +25,18 @@ let progressbar = [
 /**
  * Initialize the application.
  */
-function initBoard() {
-    loadTasks();
-    loadContactsForTasks();
+async function initBoard() {
+    await loadTasks();
+    await loadContactsForTasks();
     renderTasksToBoard();
 }
 
 function renderTasksToBoard() {
-    let board = document.getElementById('board');
-    board.innerHTML = '';
-
+    document.getElementById('board').innerHTML = '';
+    
+    createColumns();
+    createHeaders();
+    createCards();
 
 }
 
@@ -42,14 +44,27 @@ function renderTasksToBoard() {
 // BOARD CONTENT
 // ****************
 
-function createColumn(i) {
+function createColumns() {
+    let board = document.getElementById('board');
 
+    for (i = 0; i < progresses.length; i++) {
+        board.innerHTML += `<div class="board-column" id="board-column-${i}"></div>`;
+    }
 }
 
-function createHeader(i) {
-
+function createHeaders() {
+    for (i = 0; i < progresses.length; i++) {
+        let progressName = progresses[i];
+        let column = document.getElementById(`board-column-${i}`);
+        column.innerHTML += `
+            <div class="board-column-header">
+                <h3>${progressName}</h3>
+                <img src="img/add.png" alt="" onclick="showAddTaskOverlay(${i}); return false">
+            </div>
+        `;
+    }
 }
 
-function createCard(i) {
+function createCards(i) {
 
 }
