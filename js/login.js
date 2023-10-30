eff4b4b6541de4a0c383a9ed486479fb201992aa
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2023-10-30 15:02:48 +0100

    added password visibility function

--- js/login.js
+++ js/login.js
@@ -1,12 +1,40 @@
-/**
- * Clears the activeUser as part of the logout event.
- * @async
- */
+// msgBox for success registration message
+const urlParams = new URLSearchParams(window.location.search);
+const msg = urlParams.get('msg');
+
 async function initLogin() {
     activUser = {
         'name': '',
     };
     loadUsers();
+
+    // Load logo animation
+    const loginLogo = document.getElementById("login-logo");
+    const loginMainContainer = document.getElementById("login-maincontainer");
+    const loginNav = document.getElementById("login-nav");
+
+    setTimeout(() => {
+        loginLogo.classList.add("move-to-top-left");
+    }, 300);
+    setTimeout(() => {
+        loginMainContainer.classList.add('blend-in');
+        loginNav.classList.add('blend-in');
+    }, 400);
+
+    // msgBox for success registration message
+    const msgBox = document.getElementById('msgBox');
+
+    if (msg) {
+        msgBox.innerHTML = msg;
+        // Fügen Sie eine CSS-Klasse hinzu, um die Einblendungsanimation zu aktivieren
+        msgBox.classList.add('slide-in');
+        // Nach 1 Sekunde die Ausblendungsanimation aktivieren
+        setTimeout(() => {
+            msgBox.classList.add('slide-out');
+        }, 1000);
+    } else {
+        msgBox.classList.add('d-none');
+    }
 }
 
 /**
@@ -53,4 +81,18 @@ function guestLogin() {
     activUser.name = 'Guest743';
     localStorage.setItem('activUser', JSON.stringify(activUser));
     window.location.href = "./summary.html";
+}
+
+/**
+ * Toggles the visibility of a password field.
+ * @param {string} fieldId - The ID of the password field to toggle.
+ */
+function togglePasswordVisibility(fieldId) {
+    let passwordField = document.getElementById(fieldId);
+
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+    } else {
+        passwordField.type = 'password';
+    }
 }
